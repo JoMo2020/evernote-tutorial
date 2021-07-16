95d22b62fbc563e0e92158a299862f5b666be4ec
Joseph St Bernard <jastbernard@gmail.com>
2021-07-16 12:21:19 -0400

    evernoteClone

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+
+
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyD3o2lhVO2NXERUdsHsORPX57JXeVav7Sk",
+    authDomain: "evernote-clone-87e71.firebaseapp.com",
+    projectId: "evernote-clone-87e71",
+    storageBucket: "evernote-clone-87e71.appspot.com",
+    messagingSenderId: "700536477492",
+    appId: "1:700536477492:web:0df452691a4381e55d7761",
+    measurementId: "G-52K8HKZ4CV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
